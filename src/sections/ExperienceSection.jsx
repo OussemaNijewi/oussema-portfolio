43503722ab58a72bb4ef167a7aa7def5aa944125
Oussema Nijewi <oussemasn@gmail.com>
2025-12-30 20:58:47 -0500

    cosmetic changes

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -119,12 +119,8 @@ const Experience = () => {
                       </div>
                       <div>
                         <h1 className="font-semibold text-3xl">{card.title}</h1>
-                        <p className="my-5 text-white-50">
-                          🗓️&nbsp;{card.date}
-                        </p>
-                        <p className="text-[#839CB5] italic">
-                          Responsibilities
-                        </p>
+                        <p className="my-5 text-white-50">{card.date}</p>
+                        <p className="text-[#839CB5] italic">{card.Tools}</p>
                         <ul className="list-disc ms-5 mt-5 flex flex-col gap-5 text-white-50">
                           {card.responsibilities.map(
                             (responsibility, index) => (
